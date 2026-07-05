078a37b134495706c3c28c87edc4e272d5ca33c8
inazumaleandro-ship-it <inazumaleandro@gmail.com>
2026-07-05 18:37:09 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://inazumaleandro-ship-it.github.io',
-  base: '/WAANE',   // ← Muito importante!
-  
-  // Se você já tem outras configurações, mantenha e só adicione o base
+  base: '/WAANE',
 });
